e9f8ca4a141190abcced5a87c771508730ca1ea8
nathanielazevedo <nathanielazevedo@hotmail.com>
2023-01-07 20:45:56 -0500

    fixing errors

--- src/server/routers/_app.ts
+++ src/server/routers/_app.ts
@@ -2,13 +2,11 @@
  * This file contains the root router of your tRPC-backend
  */
 import { publicProcedure, router } from '../trpc';
-import { postRouter } from './post';
 import { wordsRouter } from './words';
 
 export const appRouter = router({
   healthcheck: publicProcedure.query(() => 'yay!'),
 
-  post: postRouter,
   words: wordsRouter,
 });
 
